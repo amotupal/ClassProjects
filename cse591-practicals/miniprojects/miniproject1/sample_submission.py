46bcde2bd47662db502d199efb1bdf1a196e630b
Anil Kumar Motupalli <amotupal@asu.edu>
2017-01-29 21:04:17 -0700

    finished coding

--- cse591-practicals/miniprojects/miniproject1/sample_submission.py
+++ cse591-practicals/miniprojects/miniproject1/sample_submission.py
@@ -1,6 +1,8 @@
 # sample_submission.py
 import numpy as np
 from sklearn.linear_model.perceptron import Perceptron
+import matplotlib.pyplot as plt
+from sklearn.cross_validation import train_test_split
 
 
 class regressor(object):
@@ -9,12 +11,16 @@ class regressor(object):
 
     Args:
         data: Is a tuple, ``(x,y)``
-              ``x`` is a two or one dimensional ndarray ordered such that axis 0 is independent 
+              ``x`` is a two or one dimensional ndarray ordered such that axis 0 is independent
               data and data is spread along axis 1. If the array had only one dimension, it implies
               that data is 1D.
-              ``y`` is a 1D ndarray it will be of the same length as axis 0 or x.   
+              ``y`` is a 1D ndarray it will be of the same length as axis 0 or x.
 
     """
+    alpha = 0.1
+    error_buffer = []
+    error_valid = []
+    l = 0.0001
 
     def __init__(self, data):
         self.x, self.y = data
@@ -25,24 +31,43 @@ class regressor(object):
         input_shape = self.x.shape
         X = np.ones((input_shape[0], input_shape[1] + 1))
         X[:, :-1] = self.x
-        print(X.shape())
+        self.x = X
+        self.w = np.concatenate((self.w, np.reshape(self.b, (1, 1))), axis=0)
+        self.error_buffer.append(get_error(self.x, self.y, self.w, self.l))
+        n = len(self.x) * 0.9
+        self.X_train, self.X_val, self.y_train, self.y_val = train_test_split(
+            self.x, self.y, test_size=0.2, random_state=40)
+        self.train()
+        plt.plot(self.error_buffer)
+        plt.plot(self.error_valid)
+        plt.show()
 
-        print(self.y)
-
-    def train(X_train, y_train):
+    def train(self):
+        for i in range(1, 1000):
+            delta = np.mean(((self.y_train - self.X_train.dot(self.w))
+                             * self.X_train) + (self.l * np.hstack(self.w)), axis=0)
+            # print(np.mean(delta).shape)
+            self.w += self.alpha * np.vstack(delta)
+            self.error_buffer.append(
+                get_error(self.X_train, self.y_train, self.w, self.l))
+            self.error_valid.append(
+                get_error(self.X_val, self.y_val, self.w, self.l))
+            print("Error at epoch " + str(i) + " :: " +
+                  str(self.error_buffer[-1]) + "Error Difference ::")
+            print(self.error_buffer[-2] - self.error_buffer[-1])
 
     def get_params(self):
-        """ 
+        """
         Method that should return the model parameters.
 
         Returns:
-            tuple of numpy.ndarray: (w, b). 
+            tuple of numpy.ndarray: (w, b).
 
         Notes:
             This code will return a random numpy array for demonstration purposes.
 
         """
-        return (self.w, self.b)
+        return (self.w[:-1], self.w[-1])
 
     def get_predictions(self, x):
         """
@@ -51,15 +76,28 @@ class regressor(object):
         Args:
             x: array similar to ``x`` in ``data``. Might be of different size.
 
-        Returns:    
-            numpy.ndarray: ``y`` which is a 1D array of predictions of the same length as axis 0 of 
-                            ``x`` 
+        Returns:
+            numpy.ndarray: ``y`` which is a 1D array of predictions of the same length as axis 0 of
+                            ``x``
         Notes:
-            Temporarily returns random numpy array for demonstration purposes.                            
+            Temporarily returns random numpy array for demonstration purposes.
         """
         # Here is where you write a code to evaluate the data and produce
         # predictions.
-        return np.random.rand(self.x.shape[1])
+        x = np.concatenate((x, np.vstack(np.ones(len(x)))), axis=1)
+        return(self.w.T * x)
+
+
+def l2_norm(W):
+    return sum([x * x for x in W])
+
+
+def get_error(X, y, w, l):
+    regularizer = l2_norm(w)
+    # print(w.shape)
+    error = y - X.dot(w)
+    return np.mean(error * error + l * 0.5 * regularizer)
+    # return sum([(y - x.T.dot(w)) ** 2 for x, y in zip(X_train, y_train)])
 
 if __name__ == '__main__':
     pass
